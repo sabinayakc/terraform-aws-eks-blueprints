2bc4aa24c17fb4db3d4d7d3972f62bbd6da95349
Sabinaya KC <kc.sabinaya@gmail.com>
2023-02-02 14:01:46 -0600

    Update main.tf

--- modules/kubernetes-addons/external-dns/main.tf
+++ modules/kubernetes-addons/external-dns/main.tf
@@ -54,21 +54,10 @@ module "helm_addon" {
     create_kubernetes_service_account   = true
     create_service_account_secret_token = try(var.helm_config["create_service_account_secret_token"], false)
     kubernetes_service_account          = local.service_account
-    irsa_iam_policies                   = concat([aws_iam_policy.external_dns.arn], var.irsa_policies)
+    irsa_iam_policies                   = var.irsa_policies
   }
 
   addon_context     = var.addon_context
   manage_via_gitops = var.manage_via_gitops
 }
 
-#------------------------------------
-# IAM Policy
-#------------------------------------
-
-resource "aws_iam_policy" "external_dns" {
-  description = "External DNS IAM policy."
-  name        = "${var.addon_context.eks_cluster_id}-${local.name}-irsa"
-  path        = var.addon_context.irsa_iam_role_path
-  policy      = data.aws_iam_policy_document.external_dns_iam_policy_document.json
-  tags        = var.addon_context.tags
-}
